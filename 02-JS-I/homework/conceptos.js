5de8b9b7ae63b3e7f021241dd99b8701b04503a4
David <daviddesignpro@gmail.com>
2022-07-28 11:10:58 -0300

    JSI-Terminado

---
+++ 02-JS-I/homework/conceptos.js
@@ -0,0 +1,11 @@
+// * Variables
+// * Strings
+// * Funciones (argumentos, `return`)
+// * Declaraciones `if`
+// * Valores booleanos (`true`, `false`)
+
+// Variables: Es un espacio de memoria reservado para guardar algun valor
+// String: Es una secuencia de 1 o mas caracteres expresados entre comillas simples o dobles
+// Funciones: Son un conjunto de instrucciones que deben tener argunmentos para realizar una tarea y retornar algo relacionado a los argunmentos
+// Declaracion "if": es un metodo para comparar valores o cualquier otra comillas
+// Valores Booleanos: Es un eperador matematico que solo tiene dos valores: verdadero o Falso
