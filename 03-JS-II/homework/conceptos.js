72ebdc05fc999ef34c0da2b1cf37dc359e7dd840
David <daviddesignpro@gmail.com>
2022-07-28 15:26:40 -0300

    JSII-Listo

---
+++ 03-JS-II/homework/conceptos.js
@@ -0,0 +1,9 @@
+// 1. En un archivo de texto separado que debes crear, escribe explicaciones de los siguientes conceptos como si se lo estuvieras explicando a un niño de 12 años. Hacer esto te ayudará a descubrir rápidamente cualquier agujero en tu comprensión.
+
+// * `for`
+// * `&&`, `||`, `!`
+
+// for: Es un ciclo que recorre unos valores dados y se detiene cuando se cumple una condicion dada
+// &&: Se entiende como "Y" Es un operador logico que agrega una segunda o mas expresiones o valores para una comparacion
+// ||: Se entiende como "O" Es un operador logico que permite que se coincida con una expresion o valor "O" con otra Expresion "O" valor
+// !: Este simbolo sirve para negar un valor o una expresion: true = true !true = false
