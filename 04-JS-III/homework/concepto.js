e11e94e4cef33a36a113bf3adf6925e14973f5ef
David <daviddesignpro@gmail.com>
2022-07-29 12:41:08 -0300

    conceptos

---
+++ 04-JS-III/homework/concepto.js
@@ -0,0 +1,8 @@
+// 1. En un archivo de texto separado que debes crear, escribe
+// explicaciones de los siguientes conceptos como si se lo estuvieras
+// explicando a un niño de 12 años. Hacer esto te ayudará a descubrir
+// rápidamente cualquier agujero en tu comprensión.
+
+// * Arrays: Es un arreglo o lista que puede contener cualquier tipo de dato (num, string, null, undefined).Arrays
+// ademas tiene un orden especifico que se puede recorrer segun su posicion y manimulas individualmente
+// cada elemento
