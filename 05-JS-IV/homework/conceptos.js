17d98a93584baa84ce1a6fd2371a413bb49dddb1
David <daviddesignpro@gmail.com>
2022-08-01 14:38:54 -0300

    conceptos

---
+++ 05-JS-IV/homework/conceptos.js
@@ -0,0 +1,9 @@
+// 1. En un archivo de texto separado que debes crear, escribe explicaciones de los siguientes conceptos
+// como si se lo estuvieras explicando a un niño de 12 años. Hacer esto te ayudará a descubrir rápidamente
+// cualquier agujero en tu comprensión.
+
+// 	* Objetos: son contenedores que pueden guardar muchos elementos y cada uno es identificado por una key o nombre. y asu vez cada key tiene propiedades unicas
+// 	* Propiedades: son las caracteriticas unicas de cada elemento del objeto
+// 	* Métodos: se llaman asi a las funciones dentro de un objeto
+// 	* Bucle `for…in`: Es un loop para iterar sobre el objeto dandole como parametros el Key y el nombre del objeto
+// 	* Notación de puntos vs notación de corchetes: son dos formas de acceder a los valores dentro de un objeto.
