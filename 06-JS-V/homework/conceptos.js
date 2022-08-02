017c55e8b80f69e83fba874d882242ee120d113e
David <daviddesignpro@gmail.com>
2022-08-02 17:29:19 -0300

    conceptos

---
+++ 06-JS-V/homework/conceptos.js
@@ -0,0 +1,8 @@
+// 1. En un archivo de texto separado que debes crear, escribe explicaciones de los siguientes conceptos
+// como si se lo estuvieras explicando a un niño de 12 años. Hacer esto te ayudará a descubrir rápidamente
+// cualquier agujero en tu comprensión.
+
+// * `prototype`: Es una forma que tienen las clases de darle acceso a todos los objetos creados con ella a
+// los metodos establecidos una sola vez en la clase.
+// * _Constructors_ (de Clases): Es una forma de crear una unica plantilla base y a partir de ella crear los
+// objetos todos con el mismo formato
